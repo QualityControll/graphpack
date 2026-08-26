e0785f111288d5082cdd88c3d57dd626159f1337
Chris Foster <fostercm2@gmail.com>
2026-08-26 09:00:48 -0400

    Move map experiment into lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,16 @@ mod op;
 pub use graph_value::GraphValue;
 pub use input::Input;
 pub use op::{Op, OpKind};
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn map_closure_can_build_graph_with_regular_constants() {
+        let x = Input::<f32>::new("x");
+        let y = x.map(|v| v * 2.0 + 1.0);
+
+        assert_eq!(y.op().kind(), &OpKind::Map);
+    }
+}
