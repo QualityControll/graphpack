5be688bdaa6cd67432e6bbf7dcf4102e90856f33
Chris Foster <fostercm2@gmail.com>
2026-08-26 09:10:52 -0400

    Fix materialization graph test

--- src/lib.rs
+++ src/lib.rs
@@ -13,13 +13,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn map_closure_can_build_graph_with_regular_constants() {
+    fn map_closure_can_be_materialized() {
         let x = Input::<f32>::new("x");
         let y = x.map(|v| v * 2.0 + 1.0);
-
         let graph = y.materialize();
 
         assert_eq!(graph.output().kind(), &OpKind::Map);
-        assert_eq!(graph.operations().len(), 5);
+        assert_eq!(graph.operations().len(), 6);
     }
 }
