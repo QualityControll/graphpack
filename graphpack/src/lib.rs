156ff9051ce88cd17001aa0362be3e8894345798
Chris Foster <fostercm2@gmail.com>
2026-08-25 21:11:12 -0400

    Fix unit GraphDef test

--- graphpack/src/lib.rs
+++ graphpack/src/lib.rs
@@ -2,12 +2,12 @@ pub use graphpack_macros::graphpack;
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::graphpack;
 
     #[test]
     fn graphpack_unit_closure_produces_empty_graph_def() {
         let graph_def = graphpack!(|| {});
 
-        assert!(graph_def.is_empty());
+        assert_eq!(graph_def.node.len(), 0);
     }
 }
