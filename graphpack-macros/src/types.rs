2385ab03158bae9ceb78fc65543dd107c7036ff7
Chris Foster <fostercm2@gmail.com>
2026-08-25 22:16:37 -0400

    Support more basic scalar input types

--- graphpack-macros/src/types.rs
+++ graphpack-macros/src/types.rs
@@ -4,8 +4,17 @@ use syn::{GenericArgument, PathArguments, Type};
 
 #[derive(Clone, Copy)]
 pub enum ScalarType {
-    F32,
+    Bool,
+    I8,
+    I16,
     I32,
+    I64,
+    U8,
+    U16,
+    U32,
+    U64,
+    F32,
+    F64,
 }
 
 impl ScalarType {
@@ -20,9 +29,18 @@ impl ScalarType {
         match &segment.arguments {
             PathArguments::AngleBracketed(arguments) if arguments.args.len() == 1 => {
                 match arguments.args.first().unwrap() {
-                    GenericArgument::Type(Type::Path(path)) if path.path.is_ident("f32") => Ok(Self::F32),
+                    GenericArgument::Type(Type::Path(path)) if path.path.is_ident("bool") => Ok(Self::Bool),
+                    GenericArgument::Type(Type::Path(path)) if path.path.is_ident("i8") => Ok(Self::I8),
+                    GenericArgument::Type(Type::Path(path)) if path.path.is_ident("i16") => Ok(Self::I16),
                     GenericArgument::Type(Type::Path(path)) if path.path.is_ident("i32") => Ok(Self::I32),
-                    other => Err(syn::Error::new_spanned(other, "graphpack! currently supports Input<f32> and Input<i32>")),
+                    GenericArgument::Type(Type::Path(path)) if path.path.is_ident("i64") => Ok(Self::I64),
+                    GenericArgument::Type(Type::Path(path)) if path.path.is_ident("u8") => Ok(Self::U8),
+                    GenericArgument::Type(Type::Path(path)) if path.path.is_ident("u16") => Ok(Self::U16),
+                    GenericArgument::Type(Type::Path(path)) if path.path.is_ident("u32") => Ok(Self::U32),
+                    GenericArgument::Type(Type::Path(path)) if path.path.is_ident("u64") => Ok(Self::U64),
+                    GenericArgument::Type(Type::Path(path)) if path.path.is_ident("f32") => Ok(Self::F32),
+                    GenericArgument::Type(Type::Path(path)) if path.path.is_ident("f64") => Ok(Self::F64),
+                    other => Err(syn::Error::new_spanned(other, "graphpack! unsupported Input<T> scalar type")),
                 }
             }
             _ => Err(syn::Error::new_spanned(&segment.arguments, "graphpack! inputs must have type Input<T>")),
@@ -31,15 +49,33 @@ impl ScalarType {
 
     pub fn data_type(self) -> TokenStream {
         match self {
-            Self::F32 => quote!(::tensorflow::DataType::Float),
+            Self::Bool => quote!(::tensorflow::DataType::Bool),
+            Self::I8 => quote!(::tensorflow::DataType::Int8),
+            Self::I16 => quote!(::tensorflow::DataType::Int16),
             Self::I32 => quote!(::tensorflow::DataType::Int32),
+            Self::I64 => quote!(::tensorflow::DataType::Int64),
+            Self::U8 => quote!(::tensorflow::DataType::UInt8),
+            Self::U16 => quote!(::tensorflow::DataType::UInt16),
+            Self::U32 => quote!(::tensorflow::DataType::UInt32),
+            Self::U64 => quote!(::tensorflow::DataType::UInt64),
+            Self::F32 => quote!(::tensorflow::DataType::Float),
+            Self::F64 => quote!(::tensorflow::DataType::Double),
         }
     }
 
     pub fn tensor_type(self) -> TokenStream {
         match self {
-            Self::F32 => quote!(f32),
+            Self::Bool => quote!(bool),
+            Self::I8 => quote!(i8),
+            Self::I16 => quote!(i16),
             Self::I32 => quote!(i32),
+            Self::I64 => quote!(i64),
+            Self::U8 => quote!(u8),
+            Self::U16 => quote!(u16),
+            Self::U32 => quote!(u32),
+            Self::U64 => quote!(u64),
+            Self::F32 => quote!(f32),
+            Self::F64 => quote!(f64),
         }
     }
 }
