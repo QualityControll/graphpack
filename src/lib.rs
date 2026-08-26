12133dcc6170bad03635d8f13b253b74975dafdd
Chris Foster <fostercm2@gmail.com>
2026-08-26 09:10:33 -0400

    Expose Graph and materialize test

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
+mod graph;
 mod graph_value;
 mod input;
 mod op;
 
+pub use graph::Graph;
 pub use graph_value::GraphValue;
 pub use input::Input;
-pub use op::{Op, OpKind};
+pub use op::{ConstantValue, Op, OpKind};
 
 #[cfg(test)]
 mod tests {
@@ -13,8 +15,11 @@ mod tests {
     #[test]
     fn map_closure_can_build_graph_with_regular_constants() {
         let x = Input::<f32>::new("x");
+        let y = x.map(|v| v * 2.0 + 1.0);
 
-        let _y = x.map(|v| v * 2.0 + 1.0);
+        let graph = y.materialize();
+
+        assert_eq!(graph.output().kind(), &OpKind::Map);
+        assert_eq!(graph.operations().len(), 5);
     }
-    
 }
