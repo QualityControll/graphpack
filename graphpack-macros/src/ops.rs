0cf5ae4580a235d184fddc3c7dcdf49df6b14c40
Chris Foster <fostercm2@gmail.com>
2026-08-25 21:58:05 -0400

    Add TensorFlow operator mapping module

---
+++ graphpack-macros/src/ops.rs
@@ -0,0 +1,29 @@
+use proc_macro2::TokenStream;
+use quote::quote;
+use syn::BinOp;
+
+pub fn binary_op(op: &BinOp) -> Option<TokenStream> {
+    let name = match op {
+        BinOp::Add(_) => "Add",
+        BinOp::Sub(_) => "Sub",
+        BinOp::Mul(_) => "Mul",
+        BinOp::Div(_) => "Div",
+        BinOp::Rem(_) => "FloorMod",
+        BinOp::BitAnd(_) => "BitwiseAnd",
+        BinOp::BitOr(_) => "BitwiseOr",
+        BinOp::BitXor(_) => "BitwiseXor",
+        BinOp::Shl(_) => "LeftShift",
+        BinOp::Shr(_) => "RightShift",
+        _ => return None,
+    };
+    Some(quote!(#name))
+}
+
+pub fn unary_op(op: &syn::UnOp) -> Option<TokenStream> {
+    let name = match op {
+        syn::UnOp::Neg(_) => "Neg",
+        syn::UnOp::Not(_) => "Invert",
+        _ => return None,
+    };
+    Some(quote!(#name))
+}
