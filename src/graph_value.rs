ac0da25e1dc48274cfd3142c3a3ee9dbdb3314a8
Chris Foster <fostercm2@gmail.com>
2026-08-26 08:53:45 -0400

    Support regular scalar constants in GraphValue operators

--- src/graph_value.rs
+++ src/graph_value.rs
@@ -21,35 +21,22 @@ impl<T> GraphValue<T> {
     }
 }
 
-impl GraphValue<f32> {
-    pub fn constant(value: f32) -> Self {
-        Self::from_op(Rc::new(Op::new(OpKind::Constant { value: ConstantValue::F32(value) }, vec![])))
-    }
+fn constant<T: IntoConstant>(value: T) -> GraphValue<T> {
+    GraphValue::from_op(Rc::new(Op::new(
+        OpKind::Constant { value: value.into_constant() },
+        vec![],
+    )))
 }
 
-impl GraphValue<f64> {
-    pub fn constant(value: f64) -> Self {
-        Self::from_op(Rc::new(Op::new(OpKind::Constant { value: ConstantValue::F64(value) }, vec![])))
-    }
+trait IntoConstant: Sized {
+    fn into_constant(self) -> ConstantValue;
 }
 
-impl GraphValue<i32> {
-    pub fn constant(value: i32) -> Self {
-        Self::from_op(Rc::new(Op::new(OpKind::Constant { value: ConstantValue::I32(value) }, vec![])))
-    }
-}
-
-impl GraphValue<i64> {
-    pub fn constant(value: i64) -> Self {
-        Self::from_op(Rc::new(Op::new(OpKind::Constant { value: ConstantValue::I64(value) }, vec![])))
-    }
-}
-
-impl GraphValue<bool> {
-    pub fn constant(value: bool) -> Self {
-        Self::from_op(Rc::new(Op::new(OpKind::Constant { value: ConstantValue::Bool(value) }, vec![])))
-    }
-}
+impl IntoConstant for f32 { fn into_constant(self) -> ConstantValue { ConstantValue::F32(self) } }
+impl IntoConstant for f64 { fn into_constant(self) -> ConstantValue { ConstantValue::F64(self) } }
+impl IntoConstant for i32 { fn into_constant(self) -> ConstantValue { ConstantValue::I32(self) } }
+impl IntoConstant for i64 { fn into_constant(self) -> ConstantValue { ConstantValue::I64(self) } }
+impl IntoConstant for bool { fn into_constant(self) -> ConstantValue { ConstantValue::Bool(self) } }
 
 fn binary_op<T>(lhs: GraphValue<T>, rhs: GraphValue<T>, kind: OpKind) -> GraphValue<T> {
     GraphValue::from_op(Rc::new(Op::new(kind, vec![lhs.op.clone(), rhs.op.clone()])))
@@ -59,22 +46,18 @@ impl<T> Add for GraphValue<T> {
     type Output = GraphValue<T>;
     fn add(self, rhs: Self) -> Self::Output { binary_op(self, rhs, OpKind::Add) }
 }
-
 impl<T> Sub for GraphValue<T> {
     type Output = GraphValue<T>;
     fn sub(self, rhs: Self) -> Self::Output { binary_op(self, rhs, OpKind::Sub) }
 }
-
 impl<T> Mul for GraphValue<T> {
     type Output = GraphValue<T>;
     fn mul(self, rhs: Self) -> Self::Output { binary_op(self, rhs, OpKind::Mul) }
 }
-
 impl<T> Div for GraphValue<T> {
     type Output = GraphValue<T>;
     fn div(self, rhs: Self) -> Self::Output { binary_op(self, rhs, OpKind::Div) }
 }
-
 impl<T> Neg for GraphValue<T> {
     type Output = GraphValue<T>;
     fn neg(self) -> Self::Output {
@@ -82,17 +65,44 @@ impl<T> Neg for GraphValue<T> {
     }
 }
 
+macro_rules! impl_scalar_ops {
+    ($($t:ty),+) => {
+        $(
+            impl Add<$t> for GraphValue<$t> {
+                type Output = GraphValue<$t>;
+                fn add(self, rhs: $t) -> Self::Output { binary_op(self, constant(rhs), OpKind::Add) }
+            }
+            impl Sub<$t> for GraphValue<$t> {
+                type Output = GraphValue<$t>;
+                fn sub(self, rhs: $t) -> Self::Output { binary_op(self, constant(rhs), OpKind::Sub) }
+            }
+            impl Mul<$t> for GraphValue<$t> {
+                type Output = GraphValue<$t>;
+                fn mul(self, rhs: $t) -> Self::Output { binary_op(self, constant(rhs), OpKind::Mul) }
+            }
+            impl Div<$t> for GraphValue<$t> {
+                type Output = GraphValue<$t>;
+                fn div(self, rhs: $t) -> Self::Output { binary_op(self, constant(rhs), OpKind::Div) }
+            }
+        )+
+    };
+}
+
+impl_scalar_ops!(f32, f64, i32, i64, bool);
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn constants_build_graph_nodes() {
-        let f32_value = GraphValue::<f32>::constant(2.5);
-        assert_eq!(f32_value.op().kind(), &OpKind::Constant { value: ConstantValue::F32(2.5) });
-        assert!(f32_value.op().inputs().is_empty());
+    fn regular_scalar_builds_constant_node() {
+        let input = GraphValue::<f32>::from_op(Rc::new(Op::new(
+            OpKind::Input { name: "x".into() }, vec![],
+        )));
+        let result = input * 2.0;
 
-        let i32_value = GraphValue::<i32>::constant(7);
-        assert_eq!(i32_value.op().kind(), &OpKind::Constant { value: ConstantValue::I32(7) });
+        assert_eq!(result.op().kind(), &OpKind::Mul);
+        assert_eq!(result.op().inputs().len(), 2);
+        assert_eq!(result.op().inputs()[1].kind(), &OpKind::Constant { value: ConstantValue::F32(2.0) });
     }
 }
