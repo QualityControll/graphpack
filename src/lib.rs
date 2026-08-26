2ffb8cc3b9a70299f9fb57be62fc3dde99c4ca26
Chris Foster <fostercm2@gmail.com>
2026-08-26 10:04:14 -0400

    Add executable comparison tests

--- src/lib.rs
+++ src/lib.rs
@@ -17,11 +17,9 @@ mod tests {
     fn run_graph<T: TensorType>(graph: &TensorFlowGraph, input_name: &str, input: Tensor<T>) -> Tensor<T> {
         let x_op: Operation = graph.operation_by_name(input_name).unwrap().unwrap();
         let output_op: Operation = graph.operation_by_name("output").unwrap().unwrap();
-
         let mut args = SessionRunArgs::new();
         args.add_feed(&x_op, 0, &input);
         let token = args.request_fetch(&output_op, 0);
-
         let session = Session::new(&SessionOptions::new(), graph).unwrap();
         session.run(&mut args).unwrap();
         args.fetch(token).unwrap()
@@ -31,10 +29,6 @@ mod tests {
     fn map_lowers_to_executable_tensorflow_graph() {
         let x = Input::<f32>::new("x");
         let graph = x.map(|v| v * 2.0 + 1.0).collect();
-
-        assert!(graph.operation_by_name("x").is_ok());
-        assert!(graph.operation_by_name("output").is_ok());
-
         let output = run_graph(&graph, "x", Tensor::from(3.0_f32));
         assert_eq!(output[0], 7.0);
     }
@@ -42,27 +36,42 @@ mod tests {
     #[test]
     fn chained_map_lowers_to_tensorflow_graph() {
         let x = Input::<f32>::new("x");
-        let graph = x
-            .map(|v| v * 2.0)
-            .map(|v| v + 1.0)
-            .map(|v| v * 3.0)
-            .collect();
-
-        assert!(graph.operation_by_name("x").is_ok());
-        assert!(graph.operation_by_name("output").is_ok());
-        assert_eq!(graph.operation_iter().count(), 7);
-
+        let graph = x.map(|v| v * 2.0).map(|v| v + 1.0).map(|v| v * 3.0).collect();
         let output = run_graph(&graph, "x", Tensor::from(3.0_f32));
         assert_eq!(output[0], 21.0);
     }
 
     #[test]
+    fn comparisons_work_in_map() {
+        let x = Input::<i32>::new("x");
+        let graph = x.map(|v| v.gt(10)).collect();
+        let output = run_graph(&graph, "x", Tensor::from(12_i32));
+        assert!(output[0]);
+    }
+
+    #[test]
+    fn all_comparisons_work_in_map() {
+        let x = Input::<i32>::new("x");
+        let graph = x.map(|v| v.ge(10)).collect();
+        assert!(run_graph(&graph, "x", Tensor::from(10_i32))[0]);
+
+        let x = Input::<i32>::new("x");
+        let graph = x.map(|v| v.le(10)).collect();
+        assert!(run_graph(&graph, "x", Tensor::from(10_i32))[0]);
+
+        let x = Input::<i32>::new("x");
+        let graph = x.map(|v| v.lt(10)).collect();
+        assert!(run_graph(&graph, "x", Tensor::from(9_i32))[0]);
+
+        let x = Input::<i32>::new("x");
+        let graph = x.map(|v| v.eq(GraphValue::from_op(std::rc::Rc::new(Op::new(OpKind::Constant { value: ConstantValue::I32(10) }, vec![]))))).collect();
+        assert!(run_graph(&graph, "x", Tensor::from(10_i32))[0]);
+    }
+
+    #[test]
     fn bitwise_operators_work_in_map() {
         let x = Input::<i32>::new("x");
-        let graph = x
-            .map(|v| ((v & 0b1111) ^ 0b0011) | 0b1000)
-            .collect();
-
+        let graph = x.map(|v| ((v & 0b1111) ^ 0b0011) | 0b1000).collect();
         let output = run_graph(&graph, "x", Tensor::from(0b0101_i32));
         assert_eq!(output[0], 0b1110);
     }
@@ -71,17 +80,13 @@ mod tests {
     fn bitwise_not_works_in_map() {
         let x = Input::<i32>::new("x");
         let graph = x.map(|v| !v).collect();
-
-        let output = run_graph(&graph, "x", Tensor::from(0_i32));
-        assert_eq!(output[0], !0_i32);
+        assert_eq!(run_graph(&graph, "x", Tensor::from(0_i32))[0], !0_i32);
     }
 
     #[test]
     fn bitwise_shifts_work_in_map() {
         let x = Input::<i32>::new("x");
         let graph = x.map(|v| (v << 2) >> 1).collect();
-
-        let output = run_graph(&graph, "x", Tensor::from(3_i32));
-        assert_eq!(output[0], 6);
+        assert_eq!(run_graph(&graph, "x", Tensor::from(3_i32))[0], 6);
     }
 }
