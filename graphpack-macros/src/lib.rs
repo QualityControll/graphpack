78cadcd51e258a1cb4be499d28755a20bcb708c6
Chris Foster <fostercm2@gmail.com>
2026-08-25 21:04:26 -0400

    Implement graphpack! unit closure parsing

--- graphpack-macros/src/lib.rs
+++ graphpack-macros/src/lib.rs
@@ -1,6 +1,21 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, ExprClosure, ReturnType};
 
 #[proc_macro]
-pub fn graphpack(_input: TokenStream) -> TokenStream {
-    TokenStream::new()
+pub fn graphpack(input: TokenStream) -> TokenStream {
+    let closure = parse_macro_input!(input as ExprClosure);
+
+    if closure.inputs.is_empty() && matches!(closure.output, ReturnType::Default) {
+        TokenStream::from(quote! {
+            ::graphpack::GraphDef::new()
+        })
+    } else {
+        syn::Error::new_spanned(
+            closure,
+            "graphpack! currently only supports unit closures",
+        )
+        .to_compile_error()
+        .into()
+    }
 }
