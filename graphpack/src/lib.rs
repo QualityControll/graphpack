0b1100d835a53a62b1c9f06f2d4ad19d3e191c75
Chris Foster <fostercm2@gmail.com>
2026-08-25 21:09:53 -0400

    Add unit test for graphpack! unit closure

--- graphpack/src/lib.rs
+++ graphpack/src/lib.rs
@@ -1,1 +1,13 @@
 pub use graphpack_macros::graphpack;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn graphpack_unit_closure_produces_empty_graph_def() {
+        let graph_def = graphpack!(|| {});
+
+        assert!(graph_def.is_empty());
+    }
+}
